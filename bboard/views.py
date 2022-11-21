a617bd8db0e8f55c1c91675c623117b1aac7e418
AugZim <augustzim93@gmail.com>
2022-11-21 10:06:20 +0400

    Used render shortcut and changed order_by to all in index view.

--- bboard/views.py
+++ bboard/views.py
@@ -5,7 +5,6 @@ from .models import Bb
 
 
 def index(request):
-    template = loader.get_template('bboard/index.html')
-    bbs = Bb.objects.order_by('-published')
+    bbs = Bb.objects.all()
     context = {'bbs': bbs}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'bboard/index.html', {'bbs': bbs})
