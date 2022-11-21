a45b91bc7e3ccab27012b634ac7c39b9344e749e
AugZim <augustzim93@gmail.com>
2022-11-21 09:35:05 +0400

    Improved index view. Created index.html.

--- bboard/views.py
+++ bboard/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Bb
 
 
 def index(request):
-    return HttpResponse('Future list of Adds')
+    s = 'List of Ads\n\n\n'
+    for bb in Bb.objects.order_by('-published'):
+        s += bb.title + '\n' + bb.content + '\n\n'
+    return HttpResponse(s, content_type='text/plain; charset=utf-8')
