001d4c8920c9b5cd1fa1c3807f5ade80331b3e40
AugZim <augustzim93@gmail.com>
2022-11-21 09:25:24 +0400

    Added Bb model to admin site and improved its representation.

--- bboard/models.py
+++ bboard/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 
 class Bb(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField(null=True, blank=True)
-    price = models.FloatField(null=True, blank=True)
-    published = models.DateTimeField(auto_now_add=True, db_index=True)
+    title = models.CharField(max_length=50, verbose_name='item')
+    content = models.TextField(null=True, blank=True, verbose_name='description')
+    price = models.FloatField(null=True, blank=True, verbose_name='price')
+    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='published')
+
+    class Meta:
+        verbose_name_plural = 'ads'
+        verbose_name = 'ad'
+        ordering = ['-published']
